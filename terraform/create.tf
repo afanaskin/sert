c3a350b33bc4939fd3013c7241bc077de559a0c0
afanaskin <afanaskin>
2022-03-21 12:40:49 +0300

    edit tf file key

--- terraform/create.tf
+++ terraform/create.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "build" {
   ami = "ami-092cce4a19b438926"
   instance_type = "t3.micro"
-  #key_name = "aws_key.pem"
+  key_name = "my-key001"
   vpc_security_group_ids = ["sg-014aa96269c6d3b0d"]
   tags = {
     Name = "bld"
@@ -25,7 +25,7 @@ resource "aws_instance" "build" {
 resource "aws_instance" "run" {
   ami = "ami-092cce4a19b438926"
   instance_type = "t3.micro"
-  #key_name = "aws_key.pem"
+  key_name = "my-key001"
   vpc_security_group_ids = ["sg-014aa96269c6d3b0d"]
   tags = {
     Name = "rn"
